36463d5755d37c0d74f54ca7700e0ca9021b712f
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-06-08 14:34:23 -0700

    small changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Container, Row, Col, Nav } from 'react-bootstrap';
+
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
+      <Nav>
+        
+      </Nav>
     </div>
   );
 }
