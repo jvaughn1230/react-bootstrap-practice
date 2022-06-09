55ed761a02175e69f6f9118bdc3edda8c451a539
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-06-09 11:49:04 -0700

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Container, Row, Col, Nav } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
+//import { Container, Row, Col, Nav } from 'react-bootstrap';
 
 
 
 function App() {
   return (
-    <div className="">
-      <Nav>
-        
-      </Nav>
-    </div>
+    <Navbar bg="dark" variant="dark" sticky="top" expand="sm" collapseOnSelect>
+      <Navbar.Brand href="#">
+        Bootstrap Grids
+      </Navbar.Brand>
+      <Navbar.Toggle></Navbar.Toggle>
+      <Navbar.Collapse>
+        <Nav>
+          <Nav.Link href='Grid1'>Grid 1</Nav.Link>
+          <Nav.Link href='Grid2'>Grid 2</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
