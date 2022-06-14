63a111b86936b5e7b46df73dbd43926243278fe5
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-06-14 07:32:58 -0700

    adding routes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
-import Home from './Routes/Home/Home';
+import Home from './Routes/Home';
 
 function App() {
   return (
