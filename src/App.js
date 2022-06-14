aa0b7ebe2622c19bcf269a25d53569da6d313ac0
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-06-13 17:01:11 -0700

    deleting comment

--- src/App.js
+++ src/App.js
@@ -6,12 +6,6 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 
-// import Navbar from 'react-bootstrap/Navbar';
-// import Nav from 'react-bootstrap/Nav'
-//import { Container, Row, Col, Nav } from 'react-bootstrap';
-
-
-
 function App() {
   return (
     <Routes>
