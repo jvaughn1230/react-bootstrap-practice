f8453590fd35e8d45a00df8550befc62fd7bc848
Jeffrey Vaughn <jvaughn1230@gmail.com>
2022-06-14 07:21:06 -0700

    correcting title

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -4,8 +4,8 @@ const Home = () => {
     return(
         <div className="flex-shrink-0 homeBG">
             <div className="main-container text-center mt-5">
-                <h1 className="text-center display-1 homeText">Bootstrap Grids</h1>
-                <h3 className="text-white">An Exercise in Bootstrap Grid Layouts</h3>
+                <h1 className="text-center display-1 homeText">React Bootstrap</h1>
+                <h3 className="text-white">An Exercise in React Bootstrap Grids & Layouts</h3>
             </div>
         </div>
     );
